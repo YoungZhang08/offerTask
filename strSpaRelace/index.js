7ca05cf78ed6f44834b8926267d83605e6a79094
zhangyangyang <895114856@qq.com>
2017-11-14 17:51:01 +0800

    字符替换(心累的原数组长度没有被改变)

--- strSpaRelace/index.js
+++ strSpaRelace/index.js
@@ -2,7 +2,6 @@
     function replaceSpace(str) {
         var tag = 0; //记录空格数量的变量,初始化为0
         var oldLen = str.length; //暂存原始字符串的长度
-        console.log('旧字符串长度为：' + oldLen);
         var newLen; //暂存新的长度值
 
         if (str == '' || str.length <= 0)
@@ -15,31 +14,25 @@
                 tag++;
             }
         }
-        console.log(arr);
 
         newLen = oldLen + tag * 2; //最后的字符串长度增加为原来的长度加空格数*2
-        console.log('新字符串的长度：' + newLen);
-
+        var newStr = new Array(newLen);
         var newPos = newLen - 1; //新字符串的指针
-        var j = 1;
 
-        for (var oldPos = oldLen - 1; oldPos >= arr[0] && newPos > oldPos; --oldPos) {
-            console.log('第' + j + '次旧字符串指针指向下标' + oldPos);
+        for (var oldPos = oldLen - 1; oldPos >= arr[0]; --oldPos) {
             if (str[oldPos] == ' ') {
-                str[newPos--] = '0';
-                str[newPos--] = '2';
-                str[newPos--] = '%';
+                newStr[newPos--] = '0';
+                newStr[newPos--] = '2';
+                newStr[newPos--] = '%';
             } else {
-                console.log('新字符串指针指向' + newPos);
-                str[newPos--] = str[oldPos];
-                console.log(str[newPos]);
+                newStr[newPos--] = str[oldPos];
             }
-            j++;
         }
-
-        for (var j = 0; j < newLen; j++)
-            console.log(str[j]);
+        for (var k = 0; k < arr[0]; k++)
+            newStr[k] = str[k];
+        newStr = newStr.join('');
+        console.log(newStr);
     }
 
-    replaceSpace("We Are Happy.");
+    replaceSpace("We Are Happy ");
 })();
