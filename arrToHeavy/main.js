e241973a516643ee8eb2e47e7fd3b5f84c0ecdc7
zhangyangyang <895114856@qq.com>
2017-12-06 11:10:43 +0800

    多维数组打平去重

--- arrToHeavy/main.js
+++ arrToHeavy/main.js
@@ -1,30 +1,47 @@
 //数组去重
 (function(){
-	arrToHeavy([1,3,6,[1,2,3],4,2]);
+	var newArr = new Array();
+	var arrDraw = arrToDraw([1,3,6,[1,[2,3]],4,2],newArr);
+	arrToHeavy(arrDraw);
 })();
 
-//打平
-function arrToDraw(arr){
+//多维数组打平
+function arrToDraw(arr,newArr){
 
 	for(var i = 0;i < arr.length;i++)
 	{
 		if(arr[i].length > 1)
-		{
-			for(var j = 0;j < arr[i].length;j++)
-				arr.push(arr[i][j]);  //将找到的嵌套数组push到原数组末尾
-			arr.splice(i,1);   //剔除原数组中的嵌套数组
-		}
-			
+			arrToDraw(arr[i],newArr);
+		else
+			newArr.push(arr[i]);
 	}
 
-	arr.sort();  //数组重排序
-	console.log(arr);
-	return arr;
+	newArr.sort();  //数组重排序
+	return newArr;
 }
+
+
+//二维数组打平
+// function arrToDraw(arr){
+
+// 	for(var i = 0;i < arr.length;i++)
+// 	{
+// 		if(arr[i].length > 1)
+// 		{
+// 			for(var j = 0;j < arr[i].length;j++)
+// 				arr.push(arr[i][j]);  //将找到的嵌套数组push到原数组末尾
+// 			arr.splice(i,1);   //剔除原数组中的嵌套数组
+// 		}
+			
+// 	}
+
+// 	arr.sort();  //数组重排序
+// 	console.log(arr);
+// 	return arr;
+// }
 
 //去重
 function arrToHeavy(arr){
-	arrToDraw(arr);
 	var array = new Array;
 	array.push(arr[0]);
 
